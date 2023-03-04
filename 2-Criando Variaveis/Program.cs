1938020dd6b364c6811bbc748142709be2ac50a1
Yago <modenayago@yahoo.com>
2023-03-04 03:31:20 -0300

    estudos parte 2

---
+++ 2-Criando Variaveis/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+class Program
+{
+    static void Main(String[] args)
+    {
+        Console.WriteLine("Projeto 2 - Criando variaveis");
+
+        //atribuindo a idade um valor inteiro
+        int idade;
+        idade = 21;
+        Console.WriteLine(idade);
+        idade = 21 - 5;
+        Console.WriteLine(idade);
+        idade = 5 * 2 - 6;
+        Console.WriteLine(idade);
+
+
+        Console.WriteLine("Tecle ENTER para fechar!");
+        Console.ReadLine();
+    }
+}
