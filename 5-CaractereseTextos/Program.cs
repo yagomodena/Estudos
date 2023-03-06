c3c49b8b30245af2efcce22b01b91fea1a6d1898
Yago <modenayago@yahoo.com>
2023-03-06 00:34:28 -0300

    Projto 5 - Caracteres e Textos chat e string

---
+++ 5-CaractereseTextos/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+class Program
+{
+    static void Main(String[] args)
+    {
+        Console.WriteLine("Projto 5 - Caracteres e Textos");
+
+        char letra = 'a';
+        Console.WriteLine(letra);
+
+        letra = (char)65;
+        Console.WriteLine(letra);
+
+        letra = (char)(65 + 1);
+        Console.WriteLine(letra);
+
+        string firstPhrase = "Alura cursos de tecnologia ";
+        Console.WriteLine(firstPhrase + 2023);
+
+        string saudacao = "Olá, meu nome é ";
+        string nome = "Rômulo ";
+        string continuacao = "e minha idade é ";
+        int idade = 100;
+        Console.WriteLine(saudacao + nome + continuacao + idade);
+
+        Console.WriteLine("Tecle ENTER para fechar!");
+        Console.ReadLine();
+
+    }
+}
