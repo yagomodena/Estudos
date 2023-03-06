3c7bb1fb47629c00ab7c559064cf0bc62db42b02
Yago <modenayago@yahoo.com>
2023-03-05 23:55:53 -0300

    Verificação de idade para auto-escola

--- 2-Criando Variaveis/Program.cs
+++ 2-Criando Variaveis/Program.cs
@@ -5,17 +5,19 @@ class Program
     static void Main(String[] args)
     {
         Console.WriteLine("Projeto 2 - Criando variaveis");
-
-        //atribuindo a idade um valor inteiro
-        Console.WriteLine("Digite sua idade:");
-        var idade = Console.ReadLine();
-        idade = 21;
-        Console.WriteLine(idade);
-        idade = 21 - 5;
-        Console.WriteLine(idade);
-        idade = 5 * 2 - 6;
-        Console.WriteLine(idade);
-
+        Console.WriteLine("====================");
+        Console.WriteLine("Digite o seu nome:");
+        string nome = Console.ReadLine();
+        Console.WriteLine($"Olá {nome}, qual sua idade?");
+        int idade = Convert.ToInt32(Console.ReadLine());
+        if(idade > 18)
+        {
+            Console.WriteLine($"{nome} sua idade é {idade}, você já pode tirar carta");
+        }
+        else
+        {
+            Console.WriteLine($"{nome} sua idade é {idade}, infelizmente você ainda não pode tirar carta!");
+        }
 
         Console.WriteLine("Tecle ENTER para fechar!");
         Console.ReadLine();
