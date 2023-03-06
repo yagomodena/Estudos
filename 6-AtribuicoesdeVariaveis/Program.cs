ee83fb27847bfe7ef8b3cd64d970e09754253113
Yago <modenayago@yahoo.com>
2023-03-06 00:58:04 -0300

    Projto 6 - Atribuições de Variáveis

---
+++ 6-AtribuicoesdeVariaveis/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+class Program
+{
+    static void Main(String[] args)
+    {
+        Console.WriteLine("Projto 6 - Atribuições de Variáveis");
+
+        int idade = 30;
+        int idadeAna = idade;
+
+        Console.WriteLine(idadeAna);
+
+        idade = 25;
+
+        string parcela_1 = "10";
+        string parcela_2 = "20";
+
+        Console.WriteLine(parcela_1 + parcela_2);
+
+        Console.WriteLine("Tecle ENTER para fechar!");
+        Console.ReadLine();
+
+    }
+}
