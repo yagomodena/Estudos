983994bb77632ee2ca9eddfbdf1ea6b9e6f7e3bb
Yago <modenayago@yahoo.com>
2023-03-06 00:22:43 -0300

    Projto 4 - Conversões e Outros Tipos DOUBLE, INT, LONG, SHORT e FLOAT

--- 3-CriandoVariaveisPontosFlutuantes/Program.cs
+++ 3-CriandoVariaveisPontosFlutuantes/Program.cs
@@ -4,9 +4,14 @@ class Program
 {
     static void Main(String[] args)
     {
+        Console.WriteLine("Projto 3 - Variaveis Ponto Flutuante");
         double salario;
         salario = 1300.50;
 
         Console.WriteLine(salario);
+
+        Console.WriteLine("Tecle ENTER para fechar!");
+        Console.ReadLine();
+
     }
 }
---
+++ 4-ConversõeseOutrosTipos/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+class Program
+{
+    static void Main(String[] args)
+    {
+        Console.WriteLine("Projto 4 - Conversões e Outros Tipos");
+
+        double salario = 3000.15;
+        Console.WriteLine(salario);
+
+        int salariointerio = (int)salario;
+        Console.WriteLine(salariointerio);
+
+        //LONG é um tipo de variavel de 64 bits
+        long x = 20000000000000;
+        Console.WriteLine(x);
+
+        short y = 15000;
+        Console.WriteLine(y);
+
+        float altura = 1.71f;
+        Console.WriteLine(altura);
+
+        Console.WriteLine("Tecle ENTER para fechar!");
+        Console.ReadLine();
+
+    }
+}
